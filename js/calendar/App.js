3a726d652fff407076126d22ffaebee828a7df31
rekela <ela.kawczak@gmail.com>
2020-06-07 00:48:55 +0200

    day picker added

---
+++ js/calendar/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calendar from "./Calendar";
+
+const App = () => {
+    return <Calendar/>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
